e4737f87686365e555065ae867db0634be8d7494
Dominic Simone <8732239+DominicSimone@users.noreply.github.com>
2022-10-16 15:48:12 -0700

    Rendering a sphere with unverified surface normals

---
+++ src/lib.rs
@@ -0,0 +1,106 @@
+pub mod objects;
+
+use std::rc::Rc;
+
+use crate::objects::{Intersectable, RayHit};
+use glam::{IVec2, Vec2, Vec3};
+
+const SKY_COLOR: u32 = rgb_u8(70, 180, 245);
+
+pub struct Ray {
+    position: Vec3,
+    direction: Vec3,
+}
+
+pub struct RayPayload {
+    hit_obj: Rc<dyn Intersectable>,
+    ray_hit: RayHit,
+}
+
+pub struct Camera {
+    pub position: Vec3,
+    pub forward: Vec3,
+    pub up: Vec3,
+}
+
+impl Camera {
+    pub fn new(position: Vec3, forward: Vec3) -> Self {
+        Self {
+            position,
+            forward: forward.normalize(),
+            up: Vec3::Y,
+        }
+    }
+
+    pub fn ray(&self, uv: Vec2) -> Ray {
+        let target = self.position + self.forward + uv.y * self.up + uv.x * self.up.cross(self.forward);
+        Ray {
+            position: self.position,
+            direction: target - self.position,
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct Scene {
+    // Move to arena allocator eventually? https://docs.rs/typed-arena/2.0.1/typed_arena/
+    // Also may have to use Rc instead of Box
+    pub objects: Vec<Rc<dyn Intersectable>>,
+}
+
+// uv (0, 0) is top left
+pub fn render(camera: &Camera, scene: &Scene, size: (usize, usize)) -> Vec<u32> {
+    let (size_x, size_y) = size;
+    let mut buffer: Vec<u32> = vec![0; size_x * size_y];
+    for y in 0..size_y {
+        for x in 0..size_x {
+            let index = (y * size_x + x) as usize;
+            let mut uv = Vec2 {
+                x: (x as f32 / size_x as f32),
+                y: y as f32 / size_y as f32,
+            };
+            uv -= Vec2::splat(0.5); // Adjust from [0, 1] to [-0.5, 0.5]
+            uv.x *= size_x as f32 / size_y as f32; // Adjust for aspect ratio of the window
+            buffer[index] = pixel(camera, scene, uv);
+        }
+    }
+    buffer
+}
+
+fn pixel(camera: &Camera, scene: &Scene, uv: Vec2) -> u32 {
+    if let Some(ray_result) = raycast(&camera.ray(uv), scene) {
+        rgb_vec(ray_result.ray_hit.surface_normal)
+    } else {
+        SKY_COLOR
+    }
+}
+
+fn raycast(ray: &Ray, scene: &Scene) -> Option<RayPayload> {
+    let mut closest_hit: Option<RayPayload> = None;
+    let min_t: f32 = f32::MAX;
+
+    for object in scene.objects.iter() {
+        if let Some(hit) = object.intersects(ray) {
+            if hit.t < min_t {
+                closest_hit = Some(RayPayload { hit_obj: object.clone(), ray_hit: hit });
+            }
+        }
+    }
+
+    closest_hit
+}
+
+fn rgb_f32(r: f32, g: f32, b: f32) -> u32 {
+    let r_u8 = (255.0 * r) as u32;
+    let g_u8 = (255.0 * g) as u32;
+    let b_u8 = (255.0 * b) as u32;
+    r_u8 << 16 | g_u8 << 8 | b_u8
+}
+
+fn rgb_vec(rgb: Vec3) -> u32 {
+    rgb_f32(rgb.x, rgb.y, rgb.z)
+}
+
+const fn rgb_u8(r: u8, g: u8, b: u8) -> u32 {
+    (r as u32) << 16 | (g as u32) << 8 | (b as u32)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::rc::Rc;
+
+use glam::Vec3;
+use minifb::{Key, Window, WindowOptions};
+use rust_ray_tracer::{render, Camera, Scene, objects::*};
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = 360;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = Window::new(
+        "Rust Ray Tracer - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions {
+            resize: true,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    let camera: Camera = Camera::new(Vec3::from([0., 0., 3.]), Vec3::from([0., 0., -1.]));
+
+    let mut scene = Scene::default();
+    scene.objects.push(Rc::new(Sphere::default()));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        let size = (window.get_size().0, window.get_size().1);
+
+        let frame = render(&camera, &scene, size);
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window.update_with_buffer(&frame, size.0, size.1).unwrap();
+    }
 }
